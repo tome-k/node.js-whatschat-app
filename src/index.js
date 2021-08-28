cc40e1cbda40106869082f28b1cec36d84c0b064
nicholas-bd <nicholas.bond.d@gmail.com>
2021-08-28 10:42:23 +0200

    Setup express server and static file added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+const path = require("path");
 const express = require("express");
+
 const app = express();
 const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
 
 app.get("/", (req, res) => {
   res.send("Hello World! Mario Full Stack JavaScript developer");
