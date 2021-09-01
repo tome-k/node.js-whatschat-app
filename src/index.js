318b4536464c953d3c867258148360f4c8f66e59
nicholas-bd <nicholas.bond.d@gmail.com>
2021-09-01 13:10:32 +0200

    added sending msg to rooms

--- src/index.js
+++ src/index.js
@@ -32,27 +32,36 @@ io.on("connection", (socket) => {
       return callback(error);
     }
     socket.join(user.room);
-    socket.emit("message", generateMessage("Welcome!"));
+    socket.emit(
+      "message",
+      generateMessage("Admin", "Welcome in Our Chat App!")
+    );
     socket.broadcast
       .to(user.room)
-      .emit("message", generateMessage(`${user.username} has Joined!`));
+      .emit(
+        "message",
+        generateMessage("Admin", `${user.username} has Joined!`)
+      );
 
     callback();
   });
 
   socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
     const filter = new Filter();
     if (filter.isProfane(message)) {
       return callback("Profanity is not allowed");
     }
-    io.to("my").emit("message", generateMessage(message));
+    io.to(user.room).emit("message", generateMessage(user.username, message));
     callback();
   });
 
   socket.on("sendLocation", (coords, callback) => {
-    io.emit(
+    const user = getUser(socket.id);
+    io.to(user.room).emit(
       "locationMessage",
       generateLocationMessage(
+        user.username,
         `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
       )
     );
@@ -64,7 +73,7 @@ io.on("connection", (socket) => {
     if (user) {
       io.to(user.room).emit(
         "message",
-        generateMessage(`${user.username} has left!`)
+        generateMessage("Admin", `${user.username} has left!`)
       );
     }
   });
